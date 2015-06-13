3bc938d4ec4d919756228782f696eaec4e8d0ff4
Aaron Jacobs <atheriel@gmail.com>
2015-06-12 22:14:23 -0400

    Finish basic type serialization implementations.
    Also cleans up the macros used to define them a little.
    
    Signed-off-by: Aaron Jacobs <atheriel@gmail.com>

--- src/serialize.rs
+++ src/serialize.rs
@@ -1,3 +1,5 @@
+use std::collections::{BTreeMap, HashMap};
+use std::hash::Hash;
 use std::io;
 
 use byteorder::{ByteOrder, BigEndian, WriteBytesExt};
@@ -40,56 +42,73 @@ pub fn to_writer<W, T>(dst: &mut W, obj: T) -> Result<(), NbtError>
 }
 
 macro_rules! nbtfmt_value {
-  ($T:ty, $method:ident, $tag:expr, $bare:expr) => (
+  ($T:ty, $method:ident, $tag:expr) => (
     impl NbtFmt for $T {
-      fn to_bare_nbt<W>(&self, dst: &mut W) -> Result<(), NbtError>
-           where W: io::Write {
+        #[inline]
+        fn to_bare_nbt<W>(&self, dst: &mut W) -> Result<(), NbtError>
+           where W: io::Write
+        {
             $method(dst, *self)
-      }
+        }
+
         #[inline] fn tag() -> u8 { $tag }
-        #[inline] fn is_bare() -> bool { $bare }
+        #[inline] fn is_bare() -> bool { true }
     }
   )
 }
 
 macro_rules! nbtfmt_ptr {
-  ($T:ty, $method:ident, $tag:expr, $bare:expr) => (
+  ($T:ty, $method:ident, $tag:expr) => (
     impl NbtFmt for $T {
-      fn to_bare_nbt<W>(&self, dst: &mut W) -> Result<(), NbtError>
-           where W: io::Write {
+        #[inline]
+        fn to_bare_nbt<W>(&self, dst: &mut W) -> Result<(), NbtError>
+           where W: io::Write
+        {
             $method(dst, self)
-      }
+        }
+
         #[inline] fn tag() -> u8 { $tag }
-        #[inline] fn is_bare() -> bool { $bare }
+        #[inline] fn is_bare() -> bool { true }
     }
   )
 }
 
 macro_rules! nbtfmt_slice {
-  ($T:ty, $method:ident, $tag:expr, $bare:expr) => (
+  ($T:ty, $method:ident, $tag:expr) => (
     impl NbtFmt for $T {
-      fn to_bare_nbt<W>(&self, dst: &mut W) -> Result<(), NbtError>
-           where W: io::Write {
+        #[inline]
+        fn to_bare_nbt<W>(&self, dst: &mut W) -> Result<(), NbtError>
+           where W: io::Write
+        {
             $method(dst, &self[..])
-      }
+        }
+
         #[inline] fn tag() -> u8 { $tag }
-        #[inline] fn is_bare() -> bool { $bare }
+        #[inline] fn is_bare() -> bool { true }
     }
   )
 }
 
-nbtfmt_value!(i8, write_bare_byte, 0x01, true);
-nbtfmt_value!(i16, write_bare_short, 0x02, true);
-nbtfmt_value!(i32, write_bare_int, 0x03, true);
-nbtfmt_value!(i64, write_bare_long, 0x04, true);
-nbtfmt_value!(f32, write_bare_float, 0x05, true);
-nbtfmt_value!(f64, write_bare_double, 0x06, true);
-nbtfmt_ptr!([i8], write_bare_byte_array, 0x07, true);
-nbtfmt_slice!(Vec<i8>, write_bare_byte_array, 0x07, true);
-nbtfmt_ptr!(str, write_bare_string, 0x08, true);
-nbtfmt_slice!(String, write_bare_string, 0x08, true);
-nbtfmt_ptr!([i32], write_bare_int_array, 0x0b, true);
-nbtfmt_slice!(Vec<i32>, write_bare_int_array, 0x0b, true);
+nbtfmt_value!(i8, write_bare_byte, 0x01);
+nbtfmt_value!(i16, write_bare_short, 0x02);
+nbtfmt_value!(i32, write_bare_int, 0x03);
+nbtfmt_value!(i64, write_bare_long, 0x04);
+nbtfmt_value!(f32, write_bare_float, 0x05);
+nbtfmt_value!(f64, write_bare_double, 0x06);
+nbtfmt_ptr!(str, write_bare_string, 0x08);
+nbtfmt_slice!(String, write_bare_string, 0x08);
+
+// For now, to handle conflicting implementations, use slices to indicate when
+// byte and int arrays should be preferred to lists.
+
+nbtfmt_ptr!([i8], write_bare_byte_array, 0x07);
+nbtfmt_ptr!([i32], write_bare_int_array, 0x0b);
+
+// FIXME: Remove this workaround and enable some way of uncommenting the lines
+// that follow.
+
+// nbtfmt_slice!(Vec<i8>, write_bare_byte_array, 0x07);
+// nbtfmt_slice!(Vec<i32>, write_bare_int_array, 0x0b);
 
 // impl<T> NbtFmt for [T] where T: NbtFmt {
 //  fn to_bare_nbt<W>(&self, dst: &mut W) -> Result<(), NbtError>
@@ -101,15 +120,43 @@ nbtfmt_slice!(Vec<i32>, write_bare_int_array, 0x0b, true);
 //     #[inline] fn is_bare() -> bool { true }
 // }
 
-// impl<T> NbtFmt for Vec<T> where T: NbtFmt {
-//  fn to_bare_nbt<W>(&self, dst: &mut W) -> Result<(), NbtError>
-//        where W: io::Write {
-        
-//          write_bare_list(dst, self.iter())
-//  }
-//     #[inline] fn tag() -> u8 { 0x09 }
-//     #[inline] fn is_bare() -> bool { true }
-// }
+// This leaves Vec<T> alone for lists (which kind of makes sense).
+
+impl<T> NbtFmt for Vec<T> where T: NbtFmt {
+    #[inline]
+    fn to_bare_nbt<W>(&self, dst: &mut W) -> Result<(), NbtError>
+       where W: io::Write
+    {
+           write_bare_list(dst, self.iter())
+    }
+
+    #[inline] fn tag() -> u8 { 0x09 }
+    #[inline] fn is_bare() -> bool { true }
+}
+
+impl<S, T> NbtFmt for HashMap<S, T> where S: AsRef<str> + Hash + Eq, T: NbtFmt {
+    #[inline]
+    fn to_bare_nbt<W>(&self, dst: &mut W) -> Result<(), NbtError>
+       where W: io::Write
+    {
+        write_bare_compound(dst, self.iter())
+    }
+
+    #[inline] fn tag() -> u8 { 0x0a }
+    #[inline] fn is_bare() -> bool { false }
+}
+
+impl<S, T> NbtFmt for BTreeMap<S, T> where S: AsRef<str>, T: NbtFmt {
+    #[inline]
+    fn to_bare_nbt<W>(&self, dst: &mut W) -> Result<(), NbtError>
+       where W: io::Write
+    {
+        write_bare_compound(dst, self.iter())
+    }
+
+    #[inline] fn tag() -> u8 { 0x0a }
+    #[inline] fn is_bare() -> bool { false }
+}
 
 #[inline]
 fn write_bare_byte<W>(dst: &mut W, value: i8) -> Result<(), NbtError>
@@ -218,13 +265,15 @@ fn write_bare_compound<'a, W, I, T, S>(dst: &mut W, values: I) -> Result<(), Nbt
 }
 
 #[test]
-fn nbt_test_struct_serialize() {
+fn serialize_basic_types() {
   struct TestStruct {
     name: String,
     health: i8,
     food: f32,
     emeralds: i16,
-    timestamp: i32
+    timestamp: i32,
+    ids: HashMap<String, i8>,
+    data: Vec<i8>
   }
 
   impl NbtFmt for TestStruct {
@@ -236,6 +285,8 @@ fn nbt_test_struct_serialize() {
             try!(self.food.to_nbt(dst, "food"));
             try!(self.emeralds.to_nbt(dst, "emeralds"));
             try!(self.timestamp.to_nbt(dst, "timestamp"));
+            try!(self.ids.to_nbt(dst, "ids"));
+            try!(self.data.to_nbt(dst, "data"));
 
             close_nbt(dst)
         }
@@ -243,11 +294,12 @@ fn nbt_test_struct_serialize() {
 
   let test = TestStruct {
     name: "Herobrine".to_string(),
-    health: 100, food: 20.0, emeralds: 12345, timestamp: 1424778774
+    health: 100, food: 20.0, emeralds: 12345, timestamp: 1424778774,
+    ids: HashMap::new(), data: vec![1, 2, 3]
   };
 
-  let mut test_encoded = Vec::new();
-  to_writer(&mut dst, test);
+  let mut dst = Vec::new();
+  to_writer(&mut dst, test).unwrap();
 
   let bytes = [
         0x0a,
@@ -273,8 +325,19 @@ fn nbt_test_struct_serialize() {
                 0x00, 0x09,
                 0x74, 0x69, 0x6d, 0x65, 0x73, 0x74, 0x61, 0x6d, 0x70,
                 0x54, 0xec, 0x66, 0x16,
+            0x0a,
+                0x00, 0x03,
+                0x69, 0x64, 0x73,
+                // No content.
+            0x00,
+            0x09,
+                0x00, 0x04,
+                0x64, 0x61, 0x74, 0x61,
+                0x01, // List type.
+                0x00, 0x00, 0x00, 0x03, // Length.
+                0x01, 0x02, 0x03, // Content.
         0x00
     ];
 
-    assert_eq!(&bytes[..], &test_encoded[..]);
+    assert_eq!(&bytes[..], &dst[..]);
 }
