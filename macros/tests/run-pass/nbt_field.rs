eaa643dd0800c8f40b083c71d6604f3cbec75435
Aaron Jacobs <atheriel@gmail.com>
2015-06-21 13:22:11 -0400

    Updates the run-pass tests to test `to_writer` & `from_reader`.
    As tuple structs and `#[nbt_field]` are not currently handled
    correctly, these tests are both failing.
    
    Signed-off-by: Aaron Jacobs <atheriel@gmail.com>

--- macros/tests/run-pass/nbt_field.rs
+++ macros/tests/run-pass/nbt_field.rs
@@ -3,10 +3,22 @@
 
 extern crate nbt;
 
-#[derive(NbtFmt)]
+use std::io::Cursor;
+use nbt::serialize::{from_reader, to_writer};
+
+#[derive(Debug, PartialEq, NbtFmt)]
 struct Simple {
-    #[nbt_field = "names"]
+    #[nbt_field = "name"]
     n: String,
 }
 
-fn main() { }
+fn main() {
+    let test = Simple { n: "Simple".to_string() };
+
+    let mut dst = Vec::new();
+    to_writer(&mut dst, &test).unwrap();
+
+    let result: Simple = from_reader(&mut Cursor::new(&dst[..])).unwrap();
+
+    assert_eq!(&test, &result);
+}
