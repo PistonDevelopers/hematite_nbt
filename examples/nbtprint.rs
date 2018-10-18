cb5036a0b76421537d8b14db9efd4a07d11bf664
Aaron Jacobs <atheriel@gmail.com>
2018-10-18 00:49:45 -0400

    Adds an example program showcasing serialization to JSON.
    Plus, this shows off the new pretty-printing of Blobs and Values.
    
    Signed-off-by: Aaron Jacobs <atheriel@gmail.com>

---
+++ examples/nbtprint.rs
@@ -0,0 +1,38 @@
+extern crate nbt;
+extern crate serde_json;
+
+use std::env;
+use std::fs;
+use std::process::exit;
+
+use nbt::Result;
+use nbt::Blob;
+
+fn run() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+    if let Some(arg) = args.into_iter().skip(1).take(1).next() {
+        let mut file = fs::File::open(&arg)?;
+        println!("================================= NBT Contents =================================");
+        let blob = Blob::from_reader(&mut file)?;
+        println!("{}", blob);
+        println!("============================== JSON Representation =============================");
+        match serde_json::to_string_pretty(&blob) {
+            Ok(json) => println!("{}", json),
+            Err(e) => {
+                eprintln!("error: {}", e);
+                exit(1)
+            },
+        }
+        Ok(())
+    } else {
+        eprintln!("error: a filename is required.");
+        exit(1)
+    }
+}
+
+fn main() {
+    if let Err(err) = run() {
+        eprintln!("error: {}", err);
+        exit(1)
+    };
+}
